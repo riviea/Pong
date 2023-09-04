fbec29b2c2f132bc672b63c4089105486335d3d2
Lee dohyun <12423098+riviea@users.noreply.github.com>
2023-09-05 01:01:00 +0900

    Modify BallController.cs
    튕기는 거 구현

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -10,13 +10,23 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(gameObject.transform.forward.ToString());
         dirVec = gameObject.transform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         gameObject.transform.position += (new Vector3(dirVec.x, dirVec.y) * speed) * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("1");
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("Touch");
+            dirVec *= new Vector3(-1, -1);
+        }
     }
 }

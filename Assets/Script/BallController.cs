fe76cbad0c5354e31be1fe64e772d91fd35a0154
Lee dohyun <12423098+riviea@users.noreply.github.com>
2023-09-04 21:11:05 +0900

    Modify BallController, PlayerController
    공 움직이도록 수정
    플레이어가 사용할 변수 추가

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    private Vector2 dirVec;
+    [SerializeField] private float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dirVec = gameObject.transform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        gameObject.transform.position += (new Vector3(dirVec.x, dirVec.y) * speed) * Time.deltaTime;
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector2 inputVec;
+    [SerializeField] private float speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
